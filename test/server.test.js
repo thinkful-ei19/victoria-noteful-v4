c413fbfdaa1a179044f570ea856d8d0534646f9f
Victoria Daug <victoria.daug@gmail.com>
2018-04-06 22:31:55 +0100

    setup CICD

--- test/server.test.js
+++ test/server.test.js
@@ -2,57 +2,93 @@
 const app = require('../server');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const { TEST_MONGODB_URI, JWT_SECRET } = require('../config');
+
+
+const User = require('../models/user');
+const seedUsers = require('../db/seed/users');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Reality Check', () => {
+describe('Server Test', function () {
+  let user;
+  let token;
 
-  it('true should be true', () => {
-    expect(true).to.be.true;
+  before(function () {
+    return mongoose.connect(TEST_MONGODB_URI)
+      .then(() => mongoose.connection.db.dropDatabase());
   });
 
-  it('2 + 2 should equal 4 (except in 1984)', () => {
-    expect(2 + 2).to.equal(4);
+  beforeEach(function () {
+    return  User.insertMany(seedUsers)
+      .then((users)=> {
+       user = users[0];
+       token = jwt.sign({ user }, JWT_SECRET, { subject: user.username });
+    });
   });
 
-});
-
-describe('Environment', () => {
-
-  it('NODE_ENV should be "test"', () => {
-    expect(process.env.NODE_ENV).to.equal('test');
+  afterEach(function () {
+    return mongoose.connection.db.dropDatabase()
+      .catch(err => console.error(err));
   });
 
-});
+  after(function () {
+    return mongoose.disconnect();
+  });
 
-describe('Basic Express setup', () => {
+  describe('Reality Check', () => {
 
-  describe('Express static', () => {
+    it('true should be true', () => {
+      expect(true).to.be.true;
+    });
 
-    it('GET request "/" should return the index page', () => {
-      return chai.request(app)
-        .get('/')
-        .then(function (res) {
-          expect(res).to.exist;
-          expect(res).to.have.status(200);
-          expect(res).to.be.html;
-        });
+    it('2 + 2 should equal 4 (except in 1984)', () => {
+      expect(2 + 2).to.equal(4);
     });
 
   });
 
-  describe('404 handler', () => {
+  describe('Environment', () => {
 
-    it('should respond with 404 when given a bad path', () => {
-      return chai.request(app)
-        .get('/bad/path')
-        .catch(err => err.response)
-        .then(res => {
-          expect(res).to.have.status(404);
-        });
+    it('NODE_ENV should be "test"', () => {
+      expect(process.env.NODE_ENV).to.equal('test');
     });
 
+  });
+
+  describe('Basic Express setup', () => {
+
+    describe('Express static', () => {
+
+      it('GET request "/" should return the index page', () => {
+        return chai.request(app)
+          .get('/')
+          .then(function (res) {
+            expect(res).to.exist;
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+          });
+      });
+
+    });
+
+    describe('404 handler', () => {
+
+      it('should respond with 404 when given a bad path', () => {
+        return chai.request(app)
+          .get('/bad/path')
+          .set('Authorization', `Bearer ${token}`)
+          .catch(err => err.response)
+          .then(res => {
+            expect(res).to.have.status(404);
+          });
+      });
+
+    });
   });
 });
